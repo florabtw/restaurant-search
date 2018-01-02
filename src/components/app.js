fad00b3b9648075fabc93feb51bc0092749cbe6b
Nick Pierson <nick@nickpierson.me>
2018-01-02 15:53:40 -0600

    remove eslint, add prettier

--- src/components/app.js
+++ src/components/app.js
@@ -2,19 +2,19 @@ import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
 export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+  /** Gets fired when the route changes.
+   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+   *	@param {string} event.url	The newly routed URL
+   */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	render() {
-		return (
-			<div id="app">
-				<p>Hello, world!</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="app">
+        <p>Hello, world!</p>
+      </div>
+    );
+  }
 }
