ce77408cdde0fe6d9eea899f0d7a73cbb46becd5
Nick Pierson <nick@nickpierson.me>
2018-01-02 15:46:46 -0600

    remove generated cruft

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,6 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
-import Home from '../routes/home';
-import Profile from '../routes/profile';
-// import Home from 'async!../routes/home';
-// import Profile from 'async!../routes/profile';
-
 export default class App extends Component {
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
@@ -19,12 +13,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
+				<p>Hello, world!</p>
 			</div>
 		);
 	}
