bcf268b07464165b7f18f24bc0389eae4da2672c
Nick Pierson <nick@nickpierson.me>
2018-01-03 18:14:35 -0600

    configure for github pages deployment

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,10 @@
-export default config => {
+export default (config, env, helpers) => {
   config.node.process = 'mock';
+
+  const { loader } = helpers.getLoadersByName(config, 'css-loader')[1];
+  loader.options.url = false;
+
+  if (env.production) {
+    config.output.publicPath = '/restaurant-search/build/';
+  }
 };
